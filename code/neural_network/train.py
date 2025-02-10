0f725f13ee32ef6d620f677cd7e9d3b0d19c12f3
Ole König <ole.koenig@cfa.harvard.edu>
2025-02-10 10:57:30 -0500

    train.py: Fix plotting of validation and training loss
    - training loss was computed as running average over the batch
      *during* training, while validation loss was calculated *after*
      each training epoch
    - led to the fact that validation loss was smaller than training loss
    - now calculate validation loss at same step as training loss, and
      overplot running training loss

--- code/neural_network/train.py
+++ code/neural_network/train.py
@@ -9,11 +9,14 @@ from neuralnetwork import pileupNN
 from data import load_and_split_dataset
 import config
 
+import numpy as np
+
 device = 'cpu'  # torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def training_loop(model, train_loader, criterion, optimizer):
     model.train()  # Set the model to training mode
     running_train_loss = 0.0
+    batch_losses = []
 
     for inputs, targets in train_loader:
         # Move data to the correct device (GPU/CPU)
@@ -28,9 +31,13 @@ def training_loop(model, train_loader, criterion, optimizer):
         loss.backward()
         optimizer.step()
 
+        batch_losses.append(loss.item())  # Store batch-wise loss
         running_train_loss += loss.item() * inputs.size(0)
 
     avg_train_loss = running_train_loss / len(train_loader.dataset)
+
+    batch_loss_variance = np.var(batch_losses)
+    print(f"   Batch Loss Variance: {batch_loss_variance:.6f}")  # Track loss variance
     return avg_train_loss
 
 def validation_loop(model, val_loader, criterion):
@@ -48,32 +55,35 @@ def validation_loop(model, val_loader, criterion):
 
 def train_model(model, train_loader, val_loader, criterion, optimizer,
                 num_epochs=10):
-
+    running_train_losses = []
     train_losses = []
     val_losses = []
 
     for epoch in range(num_epochs):
-        avg_train_loss = training_loop(model, train_loader, criterion, optimizer)
-        train_losses.append(avg_train_loss)
+        running_train_loss = training_loop(model, train_loader, criterion, optimizer)
+        running_train_losses.append(running_train_loss)
 
         avg_val_loss = validation_loop(model, val_loader, criterion)
         val_losses.append(avg_val_loss)
-        print(f"Epoch [{epoch + 1}/{num_epochs}], Train Loss: {avg_train_loss:.1f}, Validation Loss: {avg_val_loss:.1f}")
+        avg_train_loss = validation_loop(model, train_loader, criterion)
+        train_losses.append(avg_train_loss)
+
+        print(f"Epoch [{epoch + 1}/{num_epochs}], Running train Loss: {running_train_loss:.1f}, Train Loss: {avg_train_loss:.1f}, Validation Loss: {avg_val_loss:.1f}")
 
     weight_file = config.DATA_NEURAL_NETWORK + "model_weights.pth"
     torch.save(model.state_dict(), weight_file)
     print(f"Best model saved to model_weights.pth")# with validation loss: {best_val_loss:.4f}")
 
-    plot_loss(train_losses, val_losses)
+    plot_loss(train_losses, val_losses, running_train_losses)
 
-def plot_loss(train_losses, val_losses):
+def plot_loss(train_losses, val_losses, running_train_losses):
     plt.figure(figsize=(10/2.54, 7/2.54))
-    plt.plot(train_losses, label="Training Loss")
-    plt.plot(val_losses, label="Validation Loss")
+    plt.plot(np.arange(len(val_losses))+0.5, running_train_losses, label="Running training Loss")
+    plt.plot(np.arange(len(val_losses))+1, train_losses, label="Training Loss")
+    plt.plot(np.arange(len(val_losses))+1, val_losses, label="Validation Loss")
     plt.xlabel('Epoch')
     plt.ylabel('MSE loss')
     plt.legend()
-    plt.grid(True)
     plt.yscale('log')
     plt.tight_layout()
     plt.savefig('loss.pdf')
@@ -82,9 +92,9 @@ def main():
     train_dataset, val_dataset, test_dataset = load_and_split_dataset()
     print(f"Number of training samples: {len(train_dataset)}")
 
-    train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=16)
-    val_loader = DataLoader(val_dataset, batch_size=256, shuffle=False, num_workers=16)
-    # test_loader = DataLoader(test_dataset, batch_size=256, shuffle=False, num_workers=16)
+    train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=16)
+    val_loader = DataLoader(val_dataset, batch_size=128, shuffle=True, num_workers=16)
+    # test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False, num_workers=16)
 
     model = pileupNN(input_size=1024, hidden_size=256, output_size=1024)
     model.to(device)
@@ -94,7 +104,7 @@ def main():
     criterion = nn.MSELoss()  # Mean Squared Error loss for regression
     optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0)
 
-    train_model(model, train_loader, val_loader, criterion, optimizer, num_epochs=10)
+    train_model(model, train_loader, val_loader, criterion, optimizer, num_epochs=16)
 
 
 if __name__ == "__main__":
