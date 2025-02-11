181839a6cecf46815743fcce40dea4253c0f2ebe
Ole König <ole.koenig@cfa.harvard.edu>
2025-02-11 08:14:54 -0500

    Add latin hypercube sampling (previously: regular grid)

--- code/sixte_simulations/ero_ml_pileup_reconstruction.py
+++ code/sixte_simulations/ero_ml_pileup_reconstruction.py
@@ -1,43 +1,71 @@
-from pileupsim import Pileupsim
-from sim_ero_pileup import write_bbody_parfile,write_slurm_script
-
+# from pileupsim import Pileupsim
+from sim_ero_pileup import write_bbody_parfile
+from scipy.stats import qmc
 import numpy as np
+import os
 
-# Define the fluxes you want to simulate, in erg/cm^2/s and in
-# energyband given by EMIN, EMAX in config.py
-fluxes = np.logspace(-10, -8, 20)
+# Run it in parallel with:
+# cat batch.sh | xargs -P 30 -I {} tcsh -c "{}"
 
-nhs = [1.0]  # [10^22 cm^-2]
-# gammas = np.linspace(1.7, 2.5, 10)
-kts = np.linspace(30, 100, 100)  # [eV]
+def generate_latin_hypercube(n_points, flux_min, flux_max, kt_min, kt_max):
+    sampler = qmc.LatinHypercube(d = 2)
+    sample = sampler.random(n = n_points)
+    l_bounds = [np.log(flux_min), kt_min]
+    u_bounds = [np.log(flux_max), kt_max]
+    sample_scaled = qmc.scale(sample, l_bounds, u_bounds)
+    sample_scaled[:,0] = np.exp(sample_scaled[:,0])
+    return sample_scaled
+
+def write_parfiles(samples):
+    """
+    Write the spectrum file. As the source flux is scaled up in the SIMPUT
+    via Src_Flux parameter, the normalization doesn't matter, and we can
+    use the same spectrum file for all fluxes and only change it if we
+    change N_H/kT
+    """
+    used_fluxes = []
+    parfiles = []
+    for sample in samples:
+        flux = sample[0]
+        if flux not in used_fluxes:
+            used_fluxes.append(flux)
+            nh = 1
+            kt = sample[1]
+            parfile = write_bbody_parfile(nh, kt)
+            parfiles.append(parfile)
+        else:
+            pass
+    return parfiles
 
 def main():
-    # cmds = []
-    total = len(fluxes) * len(nhs) * len(kts)
-    counter = 0
+    # Define the fluxes you want to simulate, in erg/cm^2/s and in
+    # energyband given by EMIN, EMAX in config.py
+    flux_min, flux_max = 1e-12, 1e-8  # [cgs]
+    kt_min, kt_max = 30, 200  # [eV]
+    nh = 1  # [10^22 cm^-2]
 
-    for nh in nhs:
-        for kt in kts:
-            # Write the spectrum file. As the source flux is scaled up in
-            # the SIMPUT via Src_Flux parameter, the normalization
-            # doesn't matter, and we can use the same spectrum file for
-            # all fluxes and only change it if we change N_H/kT
-            parfile = write_bbody_parfile(nh, kt)
+    n_points = 10000
+    samples = generate_latin_hypercube(n_points, flux_min, flux_max, kt_min, kt_max)
+    parfiles = write_parfiles(samples)
 
-            for flux in fluxes:
-                print(f"Counter: {counter+1}/{total}, flux: {flux} cgs, N_H = {nh} e22 cm-2, kt = {kt} eV")
-                pileupsim = Pileupsim(flux = flux, parfile = parfile, background = "no",
-                                      verbose = -1, clobber = "yes")
+    cmds = []
+    for idx, (sample, parfile) in enumerate(zip(samples, parfiles), start=1):
+        flux = sample[0]
+        kt = sample[1]
+        print(f"{idx}/{n_points}, flux: {flux} cgs, N_H = {nh} e22 cm-2, kt = {kt} eV, parfiles = {os.path.basename(parfile)}")
 
-                pileupsim.create_simput()
-                pileupsim.run_sixtesim()
-                pileupsim.run_makespec()
-                pileupsim.clean_up()
+        #pileupsim = Pileupsim(flux = flux, parfile = parfile, background = "no",
+        #                      verbose = -1, clobber = "yes")
+        #pileupsim.create_simput()
+        #pileupsim.run_sixtesim()
+        #pileupsim.run_makespec()
+        #pileupsim.clean_up()
 
-                #cmds.append(f"python3 {os.getcwd()}/run.py --flux={flux} --parfile={parfile}")
-                counter += 1
+        cmds.append(f"python3 {os.getcwd()}/run.py --flux={flux} --parfile={parfile} --verbose=-1")
 
-    #write_slurm_script(cmds)
+    with open("batch.sh", 'w') as fp:
+        for cmd in cmds:
+            fp.write(cmd + "\n")
 
 if __name__ == "__main__":
     main()
