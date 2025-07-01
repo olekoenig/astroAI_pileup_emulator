77569a95dd36787b30ccd768b588245eada7c745
Ole König <ole.koenig@cfa.harvard.edu>
2025-07-01 07:51:37 -0400

    config.py: Use dataclasses instead of plain variables

--- code/config.py
+++ code/config.py
@@ -1,70 +1,75 @@
-"""
-Configuration file for eROSITA pile-up simulations with SIXTE.
-"""
-
 import os
+from dataclasses import dataclass
 
+@dataclass(frozen=True)
+class MLConfig:
+    data_neural_network: str = "/pool/burg1/astroai/pileup/data_neural_network/"
 
-ACTIVE_TMS = ["1", "2", "3", "4", "6"]  #: Numbers of active TMs in the real observation
-MJDREF = 51543.875  #: [days]
-DT = 0.5  #: Time resolution [s]
+    # Hyperparameters of neural network and related parameters
+    dataloader_random_seed: int = 42
+    dim_input_parameters: int = 1024  # channels in spectrum
+    dim_output_parameters: int = 1024
 
-RA = 77.283792  #257.283792-180 due to definition in SIXTE  # [decimal degrees]
-DEC = -37.511361  # [decimal degrees]
-SRCREG = os.getenv("HOME") + "/work/sources/gloria_novae/V1710Sco_src.reg"
-BKGREG = os.getenv("HOME") + "/work/sources/gloria_novae/V1710Sco_bkg.reg"
+@dataclass(frozen=True)
+class SIXTEConfig():
+    """
+    Configuration parameters for eROSITA pile-up simulations with SIXTE.
+    """
+    ACTIVE_TMS = ["1", "2", "3", "4", "6"]  #: Numbers of active TMs in the real observation
+    MJDREF = 51543.875  #: [days]
+    DT = 0.5  #: Time resolution [s]
 
-# Path to the measured eventfile as produced by eSASS' evtool (used to
-# create attitude file)
-ERO_EVENTFILE = "/userdata/data/koenig/novae4ole/V1710Sco_em04.evt"
+    RA = 77.283792  #257.283792-180 due to definition in SIXTE  # [decimal degrees]
+    DEC = -37.511361  # [decimal degrees]
+    SRCREG = os.getenv("HOME") + "/work/sources/gloria_novae/V1710Sco_src.reg"
+    BKGREG = os.getenv("HOME") + "/work/sources/gloria_novae/V1710Sco_bkg.reg"
 
-# Systematic uncertainty assigned to the spectrum due to pile-up modeling
-SYS_ERR = 0.1
+    # Path to the measured eventfile as produced by eSASS' evtool (used to
+    # create attitude file)
+    ERO_EVENTFILE = "/userdata/data/koenig/novae4ole/V1710Sco_em04.evt"
 
-# Define energy range for SIMPUT creation (source flux range)
-EMIN = 0.2  #: [keV]
-EMAX = 2.0  #: [keV]
+    # Systematic uncertainty assigned to the spectrum due to pile-up modeling
+    SYS_ERR = 0.1
 
-# Parameters specific to the data paths and computational setup
-XMLDIR = os.getenv("HOME") + "/git/software/sixte_gits/instruments/srg/instruments/srg/erosita/"
+    # Define energy range for SIMPUT creation (source flux range)
+    EMIN = 0.2  #: [keV]
+    EMAX = 2.0  #: [keV]
 
-# Folders of temporary data products (somewhere with high-speed I/O!).
-# If a big grid is calculated some products should to be deleted after each run.
-# The grid simulation is sped up if one writes these files onto a local disk.
-SCRATCHDIR = os.getenv("HOME") + "/scratch/"
-SIMPUTDIR = SCRATCHDIR + "simput/"
-EVTDIR = SCRATCHDIR + "evt/"
-ESASSDIR = SCRATCHDIR + "esass/"
+    # Parameters specific to the data paths and computational setup
+    XMLDIR = os.getenv("HOME") + "/git/software/sixte_gits/instruments/srg/instruments/srg/erosita/"
 
-# Handling of the pfiles is important only when multiple simulations
-# are done in parallel.
-PFILESDIR = SCRATCHDIR + "pfiles/"
-HEASOFT_PFILES = os.getenv("HEADAS") + "/syspfiles"
-GLOBAL_PFILES_DIR = os.getenv("HOME") + "/pfiles"
+    # Folders of temporary data products (somewhere with high-speed I/O!).
+    # If a big grid is calculated some products should to be deleted after each run.
+    # The grid simulation is sped up if one writes these files onto a local disk.
+    SCRATCHDIR = os.getenv("HOME") + "/scratch/"
+    SIMPUTDIR = SCRATCHDIR + "simput/"
+    EVTDIR = SCRATCHDIR + "evt/"
+    ESASSDIR = SCRATCHDIR + "esass/"
 
-# Folders of the data products on global disk (these products are not deleted)
-DATADIR = "/pool/burg1/astroai/pileup/sims/"
-LCDIR = DATADIR + "lc/"
-IMGDIR = DATADIR + "img/"
-LOGDIR = DATADIR + "log/"
-SPECDIR = DATADIR + "spec_piledup/"
-TARGET_SPECDIR = DATADIR + "spec_nonpiledup/"
-ISISPARFILEDIR = DATADIR + "pars/"
-DATA_NEURAL_NETWORK = "/pool/burg1/astroai/pileup/data_neural_network/"
+    # Handling of the pfiles is important only when multiple simulations
+    # are done in parallel.
+    PFILESDIR = SCRATCHDIR + "pfiles/"
+    HEASOFT_PFILES = os.getenv("HEADAS") + "/syspfiles"
+    GLOBAL_PFILES_DIR = os.getenv("HOME") + "/pfiles"
 
-# Hyperparameters of neural network and related parameters
-DATALOADER_RANDOM_SEED = 42
-DIM_OUTPUT_PARAMETERS = 3
+    # Folders of the data products on global disk (these products are not deleted)
+    DATADIR = "/pool/burg1/astroai/pileup/sims/"
+    LCDIR = DATADIR + "lc/"
+    IMGDIR = DATADIR + "img/"
+    LOGDIR = DATADIR + "log/"
+    SPECDIR = DATADIR + "spec_piledup/"
+    TARGET_SPECDIR = DATADIR + "spec_nonpiledup/"
+    ISISPARFILEDIR = DATADIR + "pars/"
 
-# If we always simulate the same slew, we don't need to always compute the
-# GTI file (ero_vis) and write RMF/ARF (srctool) but can calculate it
-# once and use it as "master files"
-PILEUPSIM_DIR = os.getenv("HOME") + "/work/sources/gloria_novae/"
-MASTER_ARF = PILEUPSIM_DIR + "srctool_master.arf"  # not needed if TODO=ALL in srctool
-MASTER_RMF = PILEUPSIM_DIR + "srctool_master.rmf"  # not needed if TODO=ALL in srctool
-ATTITUDE_FILE = PILEUPSIM_DIR + "master.att"  # created by write_attitude_and_gti
-MASTER_GTI = PILEUPSIM_DIR + "master.gti"  # write_attitude_and_gti
-ISISPREP = "none"
+    # If we always simulate the same slew, we don't need to always compute the
+    # GTI file (ero_vis) and write RMF/ARF (srctool) but can calculate it
+    # once and use it as "master files"
+    PILEUPSIM_DIR = os.getenv("HOME") + "/work/sources/gloria_novae/"
+    MASTER_ARF = PILEUPSIM_DIR + "srctool_master.arf"  # not needed if TODO=ALL in srctool
+    MASTER_RMF = PILEUPSIM_DIR + "srctool_master.rmf"  # not needed if TODO=ALL in srctool
+    ATTITUDE_FILE = PILEUPSIM_DIR + "master.att"  # created by write_attitude_and_gti
+    MASTER_GTI = PILEUPSIM_DIR + "master.gti"  # write_attitude_and_gti
+    ISISPREP = "none"
 
-# Image size of evtool output
-IMGSIZE = 70  #: pixel, default: 1024
+    # Image size of evtool output
+    IMGSIZE = 70  #: pixel, default: 1024
